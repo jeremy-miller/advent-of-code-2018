4effff16ce139ced321c69c0d69ae14620cc342f
Jeremy Miller <jgmiller88@gmail.com>
2019-05-13 07:59:30 -0700

    Add day1 code

--- day1/Main.hs
+++ day1/Main.hs
@@ -1,4 +1,30 @@
 module Main where
 
+import           Control.Monad.State
+import           Data.Foldable
+
+data Frequency = Add Int | Subtract Int
+
+parseLine :: String -> Frequency
+parseLine ('+' : frequency) = Add $ read frequency
+parseLine ('-' : frequency) = Subtract $ read frequency
+parseLine _                 = error "Invalid line"
+
+combine :: Frequency -> Int -> Int
+combine (Add      x) n = n + x
+combine (Subtract x) n = n - x
+
+adjustSingleFrequency :: String -> State Int ()
+adjustSingleFrequency line = do
+  currentFrequency <- get
+  let lineFrequency = parseLine line
+  put (combine lineFrequency currentFrequency)
+
+adjustFrequency :: [String] -> State Int ()
+adjustFrequency = traverse_ adjustSingleFrequency
+
 main :: IO ()
-main = putStrLn "Hello World"
+main = do
+  f <- readFile "data/frequencies.txt"
+  let frequencies = lines f
+  print $ execState (adjustFrequency frequencies) 0
