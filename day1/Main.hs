c22cc34f9ae2535cdfab61d7b73309c0d900b3d8
Jeremy Miller <jgmiller88@gmail.com>
2019-05-13 08:46:59 -0700

    Add variables and types to improve readability

--- day1/Main.hs
+++ day1/Main.hs
@@ -3,28 +3,30 @@ module Main where
 import           Control.Monad.State
 import           Data.Foldable
 
-data Frequency = Add Int | Subtract Int
+type Frequency = Int
+data FrequencyLine = Add Frequency | Subtract Frequency
 
-parseLine :: String -> Frequency
+parseLine :: String -> FrequencyLine
 parseLine ('+' : frequency) = Add $ read frequency
 parseLine ('-' : frequency) = Subtract $ read frequency
 parseLine _                 = error "Invalid line"
 
-combine :: Frequency -> Int -> Int
+combine :: FrequencyLine -> Frequency -> Frequency
 combine (Add      x) n = n + x
 combine (Subtract x) n = n - x
 
-adjustSingleFrequency :: String -> State Int ()
+adjustSingleFrequency :: String -> State Frequency ()
 adjustSingleFrequency line = do
   currentFrequency <- get
   let lineFrequency = parseLine line
   put (combine lineFrequency currentFrequency)
 
-adjustFrequency :: [String] -> State Int ()
+adjustFrequency :: [String] -> State Frequency ()
 adjustFrequency = traverse_ adjustSingleFrequency
 
 main :: IO ()
 main = do
-  f <- readFile "data/frequencies.txt"
-  let frequencies = lines f
-  print $ execState (adjustFrequency frequencies) 0
+  file <- readFile "data/frequencies.txt"
+  let frequencies       = lines file
+  let startingFrequency = 0
+  print $ execState (adjustFrequency frequencies) startingFrequency
